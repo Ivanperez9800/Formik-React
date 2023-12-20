0d117ddad4cfa23f268101fe526453f1f89dcd60
Ivan Perez <ivan_perez0980@proton.me>
2023-12-19 22:30:03 -0300

    Preparando para usar firebase

---
+++ formik-practica/src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX7cQnafNdHnm3Y8OgOq7YPOGWn36ZdgM",
+  authDomain: "currifacil-13013.firebaseapp.com",
+  projectId: "currifacil-13013",
+  storageBucket: "currifacil-13013.appspot.com",
+  messagingSenderId: "151326358232",
+  appId: "1:151326358232:web:f1faa700e5f223c8c0b5ad"
+};
+
+
+const app = initializeApp(firebaseConfig);
