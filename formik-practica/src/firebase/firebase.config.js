5df642b3c2877aaef759d9df497964ea91cf7084
Ivan Perez <ivan_perez0980@proton.me>
2023-12-23 20:13:26 -0300

    probando el .env

--- formik-practica/src/firebase/firebase.config.js
+++ formik-practica/src/firebase/firebase.config.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import 'dotenv/config'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "currifacil-13013.firebaseapp.com",
-  projectId: "currifacil-13013",
-  storageBucket: "currifacil-13013.appspot.com",
-  messagingSenderId: "151326358232",
-  appId: "1:151326358232:web:f1faa700e5f223c8c0b5ad"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
