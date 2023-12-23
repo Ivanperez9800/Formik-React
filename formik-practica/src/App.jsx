46d2e36bca434c47d4b692321dcf8a217c0aecfe
Ivan Perez <ivan_perez0980@proton.me>
2023-12-23 20:20:46 -0300

    areglado detalles

--- formik-practica/src/App.jsx
+++ formik-practica/src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import { BrowserRouter } from "react-router-dom";
 import SignUpForm from './Pages/SignUpForm';
-import LogInForm from './pages/LogInForm';
+import LogInForm from './Pages/LogInForm';
 import AuthProvider from './contexts/Authtentication';
 
-import ProtectedRoutes from  './components/ProtectedRoutes'
+import ProtectedRoutes from './components/ProtectedRoutes'
 import Home from './components/Home'
 
 function App() {
@@ -19,14 +19,15 @@ function App() {
         <BrowserRouter>
           <Routes>
 
-          <Route path="/" element={
-            <ProtectedRoutes>
-              <Home />
-            </ProtectedRoutes>
+            <Route path="/" element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
 
-          } />
+            } />
 
             <Route path="/login" element={<LogInForm />} />
+
             <Route path="/register" element={<SignUpForm />} />
             {/* La siguiente línea establece "/Login" como la página inicial */}
             {/* <Route index element={<LogInForm />} /> */}
