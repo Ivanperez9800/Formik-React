0d81b37ef3d4582fa9cf85cc94f2ef1c524e5079
Ivan Perez <ivan_perez0980@proton.me>
2023-12-24 11:32:14 -0300

    poniendo las variables de entorno seguras

--- formik-practica/src/firebase/firebase.config.js
+++ formik-practica/src/firebase/firebase.config.js
@@ -3,15 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDX7cQnafNdHnm3Y8OgOq7YPOGWn36ZdgM",
-  authDomain: "currifacil-13013.firebaseapp.com",
-  projectId: "currifacil-13013",
-  storageBucket: "currifacil-13013.appspot.com",
-  messagingSenderId: "151326358232",
-  appId: "1:151326358232:web:f1faa700e5f223c8c0b5ad"
+  apiKey: import.meta.env.VITE_REACT_APP_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_ID
 };
-
-
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- formik-practica/vite.config.js
+++ formik-practica/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Enable Vite's built-in support for .env files
+  envDir: './',
 })
