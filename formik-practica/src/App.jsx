13f71153b492c65b31e2adf2bc275355abf64576
Ivan Perez <ivan_perez0980@proton.me>
2023-12-25 22:35:09 -0300

    no se x2

--- formik-practica/src/App.jsx
+++ formik-practica/src/App.jsx
@@ -8,8 +8,9 @@ import AuthProvider from './contexts/Authtentication';
 import ProtectedRoutes from './components/ProtectedRoutes'
 import Home from './components/Home'
 
-import LogInForm from './pages/LogInForm';
+
 import SignUpForm from "./pages/SignUpForm";
+import LogInForm from "./pages/LogInForm";
 
 function App() {
 
